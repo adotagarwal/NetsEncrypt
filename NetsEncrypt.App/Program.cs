a5bb2e687d8468a39c41e5d0bb69dea5763c51b3
austin agarwal <aagarwal@echocap.com>
2020-01-29 15:22:14 -0500

    reorganizing demo program

--- NetsEncrypt.App/Program.cs
+++ NetsEncrypt.App/Program.cs
@@ -18,19 +18,18 @@ namespace NetsEncrypt.App
         /// <returns></returns>
         public static async Task Main()
         {
-            var path = Path.GetTempPath();
-            var userAccount = "johndoe@testdomain.com";
-            var domainToGenerate = "testdomain.com";
-
             // get an IDnsUpdater
             IDnsUpdater dnsUpdater = new PowerShellWindowsDnsUpdater();
-
             // get a client with the specified account
             IACMEClient client = new ACMEWebAPIClient(ACMEWebAPIClient.STAGE_API_ENDPOINT);
-            
             // get a wrapper which will automate the process of obtaining a certificate
             var wrapper = new CertificateGenerator(client, dnsUpdater);
 
+            // parameters
+            var path = Path.GetTempPath();
+            var userAccount = "johndoe@testdomain.com";
+            var domainToGenerate = "testdomain.com";
+            
             var x = await wrapper.SaveNewWildCardCertificate(userAccount, domainToGenerate, path);
             Console.WriteLine($"Saved certificate to: {x}");
         }
